b31107739b8ea34cd6007a3a27696e173b3a16dc
inmny <1342885270@qq.com>
2024-12-09 00:21:37 +0800

    简化ElementRootAsset

--- Source/Core/Libraries/ElementRootAsset.cs
+++ Source/Core/Libraries/ElementRootAsset.cs
@@ -4,22 +4,21 @@ namespace Cultiway.Core.Libraries;
 
 public class ElementRootAsset : Asset
 {
-    public readonly BaseStats          base_stats;
+    public readonly BaseStats base_stats = new();
     public readonly ElementComposition composition;
-    private         string             desc_key;
 
-    private string name_key;
+
+    private string f_desc_key;
+    private string f_name_key;
 
     public ElementRootAsset(string id, ElementComposition composition)
     {
         this.id = id;
         this.composition = composition;
-
-        name_key = $"er_{id}";
-        desc_key = $"er_info_{id}";
-
-        base_stats = new();
     }
+
+    private string desc_key => f_desc_key ??= $"er_info_{id}";
+    private string name_key => f_name_key ??= $"er_{id}";
 
     public override string ToString()
     {
