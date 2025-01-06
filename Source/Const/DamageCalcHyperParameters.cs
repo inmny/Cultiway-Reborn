5c8225fd20f89161df44a16b9b013020d32d33d0
inmny <1342885270@qq.com>
2025-01-06 20:51:58 +0800

    数值调整

--- Source/Const/DamageCalcHyperParameters.cs
+++ Source/Const/DamageCalcHyperParameters.cs
@@ -2,7 +2,7 @@ namespace Cultiway.Const;
 
 public static class DamageCalcHyperParameters
 {
-    public const float ArmorEffectDecay  = 10000;
-    public const float MasterEffectDecay = 10000;
+    public const float ArmorEffectDecay  = 100;
+    public const float MasterEffectDecay = 100;
     public const float PowerBase = 100;
 }
