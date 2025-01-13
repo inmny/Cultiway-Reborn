ad76a552a7bb33acedb5f1d79e15be5f4920afb2
inmny <1342885270@qq.com>
2025-01-13 22:00:03 +0800

    去除一些没用会导致报错的ui

--- Source/Patch/PatchCityWindow.cs
+++ Source/Patch/PatchCityWindow.cs
@@ -17,12 +17,6 @@ internal static class PatchCityWindow
 {
     private static bool                              _initialized;
     private static Text                              info_text;
-    private static Action<CityExtend, StringBuilder> info_display_func;
-
-    public static void RegisterInfoDisplay(Action<CityExtend, StringBuilder> func)
-    {
-        info_display_func += func;
-    }
 
     [HarmonyPrefix, Hotfixable]
     [HarmonyPatch(typeof(CityWindow), nameof(CityWindow.OnEnable))]
@@ -47,41 +41,6 @@ internal static class PatchCityWindow
 
         StringBuilder sb = new();
         sb.AppendLine(__instance.city.data.id);
-        CityExtend city_extend = __instance.city.GetExtend();
-
-        var items = city_extend.GetSpecialItems();
-        sb.AppendLine("Special Items:");
-        var idx = 0;
-        foreach (SpecialItem item in items)
-        {
-            Entity item_entity = item.self;
-            sb.Append($"[{idx++}] {item_entity}: ");
-            if (item_entity.TryGetComponent(out ItemShape shape))
-            {
-                sb.Append(shape.shape_id);
-                sb.Append('\t');
-            }
-
-            if (item_entity.TryGetComponent(out Jindan jindan))
-            {
-                sb.Append(jindan.jindan_type);
-                sb.Append('\t');
-            }
-
-            if (item_entity.TryGetComponent(out XianBase xian_base))
-            {
-                sb.Append(xian_base);
-                sb.Append('\t');
-            }
-
-            if (item_entity.TryGetComponent(out ElementRoot element_root))
-            {
-                sb.Append(element_root.Type.id);
-                sb.Append('\n');
-            }
-        }
-
-        info_display_func?.Invoke(city_extend, sb);
         info_text.text = sb.ToString();
     }
 }
