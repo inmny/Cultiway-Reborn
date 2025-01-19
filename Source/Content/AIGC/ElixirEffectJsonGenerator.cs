fcdafdd8ce98f1ff8bfbb9b9bf01929070618171
inmny <1342885270@qq.com>
2025-01-20 00:36:30 +0800

    基本完成丹药效果生成的prompt

--- Source/Content/AIGC/ElixirEffectJsonGenerator.cs
+++ Source/Content/AIGC/ElixirEffectJsonGenerator.cs
@@ -2,6 +2,7 @@ using System.IO;
 using System.Text;
 using Cultiway.Core.AIGCLib;
 using HarmonyLib;
+using NeoModLoader.api.attributes;
 using UnityEngine;
 
 namespace Cultiway.Content.AIGC;
@@ -14,7 +15,7 @@ public class ElixirEffectJsonGenerator : PromptNameGenerator<ElixirEffectJsonGen
     {
         return string.Empty;
     }
-
+    [Hotfixable]
     protected override bool RequestNewName(string key)
     {
         return !NameDict.TryGetValue(key, out var names) || Toolbox.randomChance(1 / Mathf.Exp(names.Count));
@@ -24,24 +25,39 @@ public class ElixirEffectJsonGenerator : PromptNameGenerator<ElixirEffectJsonGen
     {
         return param.Join();
     }
-
+    [Hotfixable]
     protected override string GetPrompt(string[] param)
     {
         StringBuilder sb = new();
         sb.Append("为由");
         for (int i=0;i<param.Length;i++)
         {
-            sb.Append('\"');
+            sb.Append('”');
             sb.Append(param[i]);
-            sb.Append('\"');
+            sb.Append('”');
             if (i < param.Length - 1)
             {
                 sb.Append(',');
             }
         }
 
-        sb.Append("制成的丹药生成药效，");
-        // TODO: 填写更完善的prompt使得其
+        sb.Append(
+            """制成的丹药生成药效，组织成json格式。要求格式与以下示例一致(注意使用英文引号)，不能新增字段，并且bonus_stats需要贴合effect_description，effect_description需要贴合丹药材料:```json{”effect_type”: ”StatusGain”,”effect_description”: ”此丹药由幻梦灵藤与幻影灵草带来虚幻力量，结合烈火灵草的炽热之力，能够使人在战斗中如幻影般难以捉摸，同时速度与攻击速度得到显著提升”,”bonus_stats”: {{”speed”: 10,”attack_speed”: 20,”mod_attack_speed”: 0.5,”mod_speed”:0.2}} }}```bonus_stats可选的key有:""");
+        sb.Append(BaseStatses.AllStatsIds);
         return sb.ToString();
+    }
+    [Hotfixable]
+    private static string TestGenerateName()
+    {
+        string[] param = new string[3];
+        for (int i=0;i<param.Length;i++)
+        {
+            param[i] = PlantNameGenerator.Instance.GenerateName([
+                ModClass.L.ElementRootLibrary.list.GetRandom().GetName(),
+                Libraries.Manager.JindanLibrary.list.GetRandom().GetName()
+            ]);
+        }
+        ModClass.LogInfo("Test generate name: " + param.Join());
+        return Instance.GenerateName(param);
     }
 }
--- Source/Content/BaseStatses.cs
+++ Source/Content/BaseStatses.cs
@@ -1,6 +1,8 @@
+using System.Text;
 using Cultiway.Abstract;
 using Cultiway.Content.Components;
 using Cultiway.Patch;
+using NeoModLoader.General.Game.extensions;
 
 namespace Cultiway.Content;
 
@@ -9,6 +11,8 @@ public class BaseStatses : ExtendLibrary<BaseStatAsset, BaseStatses>
 {
     [AssetId(nameof(MaxWakan))]public static BaseStatAsset MaxWakan { get; private set; }
     [AssetId(nameof(WakanRegen))] public static BaseStatAsset WakanRegen { get; private set; }
+    private static StringBuilder all_stats_ids = new();
+    internal static string AllStatsIds => all_stats_ids.ToString();
 
     protected override void OnInit()
     {
@@ -18,6 +22,18 @@ public class BaseStatses : ExtendLibrary<BaseStatAsset, BaseStatses>
             if (!ae.HasCultisys<Xian>()) return;
             var wakan = ae.GetCultisys<Xian>().wakan;
             sb.AppendLine($"灵气: {wakan} / {ae.Base.stats[MaxWakan.id]}");
+        });
+        
+        AssetManager.base_stats_library.ForEach<BaseStatAsset, BaseStatsLibrary>(asset =>
+        {
+            if (all_stats_ids.Length > 0)
+            {
+                all_stats_ids.Append(',');
+            }
+
+            all_stats_ids.Append('”');
+            all_stats_ids.Append(asset.id);
+            all_stats_ids.Append('”');
         });
     }
 }
