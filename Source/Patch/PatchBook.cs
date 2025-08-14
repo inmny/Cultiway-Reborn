946a1b5ca4dd79e781ba710764a82d2e01dbbef5
inmny <1342885270@qq.com>
2025-08-15 00:19:55 +0800

    feat(core): 添加自定义书籍封面功能
    - 新增 BookTypeAssetExtend 类，用于存储自定义封面名称
    - 实现 PatchBook 类，通过 Harmony 补丁修改书籍创建逻辑
    - 添加自定义封面路径获取方法，支持随机选择封面图片
    - 使用字典缓存自定义封面图片列表，提高性能

---
+++ Source/Patch/PatchBook.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using Cultiway.Core;
+using Cultiway.Utils.Extension;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Cultiway.Patch;
+
+internal static class PatchBook
+{
+    [HarmonyPostfix, HarmonyPatch(typeof(Book), nameof(Book.newBook))]
+    private static void newBook_postfix(Book __instance, BookTypeAsset pBookType)
+    {
+        var bte = pBookType.GetExtend<BookTypeAssetExtend>();
+        if (!string.IsNullOrEmpty(bte.custom_cover_name))
+        {
+            __instance.data.path_cover = GetCustomCoverPath(bte.custom_cover_name);
+        }
+    }
+
+    private static string GetCustomCoverPath(string custom_cover_name)
+    {
+        if (!_cache_custom_covers.TryGetValue(custom_cover_name, out var names))
+        {
+            names = SpriteTextureLoader.getSpriteList($"books/custom_book_covers/{custom_cover_name}")
+                .Select(x => x.name).ToList();
+            _cache_custom_covers.Add(custom_cover_name, names);
+        }
+        return $"../custom_book_covers/{custom_cover_name}/{names.GetRandom()}";
+    }
+    private static Dictionary<string, List<string>> _cache_custom_covers = new();
+}
