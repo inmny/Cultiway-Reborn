24556f47718ea397af1711ea7b86b6f0a817b5ad
inmny <1342885270@qq.com>
2025-08-20 14:08:00 +0800

    refactor(BookManagerTools): 功法属性加成只考虑主要属性

--- Source/Content/Extensions/BookManagerTools.cs
+++ Source/Content/Extensions/BookManagerTools.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Collections.Generic;
 using Cultiway.Content.Components;
 using Cultiway.Core.Components;
 using Cultiway.Utils.Extension;
+using UnityEngine;
 
 namespace Cultiway.Content.Extensions;
 
@@ -16,15 +19,50 @@ public static class BookManagerTools
         }
         var be = raw_cultibook.GetExtend();
         var stats = new BaseStats();
-        foreach (var stat in WorldboxGame.BaseStats.ArmorStats)
-        {
-            stats[stat] = creator.stats[stat];
-        }
 
-        foreach (var stat in WorldboxGame.BaseStats.MasterStats)
+        void SoftmaxStats(IList<string> stat_ids)
         {
-            stats[stat] = creator.stats[stat];
+            (string, float, float)[] armor_stats = new (string, float, float)[stat_ids.Count];
+            var exp_sum = 0f;
+            var max_val = 0f;
+            for (int i = 0; i < armor_stats.Length; i++)
+            {
+                var stat_id = stat_ids[i];
+                var stat_value = creator.stats[stat_id];
+                max_val = Mathf.Max(max_val, stat_value);
+                armor_stats[i] = (stat_id, stat_value, 0f);
+            }
+
+            for (int i = 0; i < armor_stats.Length; i++)
+            {
+                var stat_value = armor_stats[i].Item2;
+                var exp_value = Mathf.Exp(stat_value - max_val);
+                armor_stats[i].Item3 = exp_value;
+                exp_sum += exp_value;
+            }
+
+            for (int i = 0; i < armor_stats.Length; i++)
+            {
+                armor_stats[i].Item3 /= exp_sum;
+            }
+            Array.Sort(armor_stats, (a, b) => b.Item3.CompareTo(a.Item3));
+            var accum_prob = 0f;
+            for (int i = 0; i < armor_stats.Length; i++)
+            {
+                var prob = armor_stats[i].Item3;
+                accum_prob += prob;
+                var stat_id = armor_stats[i].Item1;
+                var stat_value = armor_stats[i].Item2;
+                stats[stat_id] = stat_value;
+                if (accum_prob > 0.9f)
+                {
+                    break;
+                }
+            }
+
         }
+        SoftmaxStats(WorldboxGame.BaseStats.ArmorStats);
+        SoftmaxStats(WorldboxGame.BaseStats.MasterStats);
         be.AddComponent(new Cultibook()
         {
             FinalStats = stats
