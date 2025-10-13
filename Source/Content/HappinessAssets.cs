d96397cd35ea109185d2439a56436c09fb0b73cd
inmny <1342885270@qq.com>
2025-10-14 00:22:41 +0800

    feat: 突破境界会带来极高的"幸福度"

---
+++ Source/Content/HappinessAssets.cs
@@ -0,0 +1,24 @@
+using Cultiway.Abstract;
+using strings;
+
+namespace Cultiway.Content;
+
+public class HappinessAssets : ExtendLibrary<HappinessAsset, HappinessAssets>
+{
+    public static HappinessAsset LevelUp { get; private set; }
+    protected override void OnInit()
+    {
+        RegisterAssets();
+
+        LevelUp.value = 100;
+        LevelUp.pot_task_id = S_Task.happy_laughing;
+        LevelUp.pot_amount = 3;
+        LevelUp.path_icon = "ui/icons/iconKings";
+    }
+
+    protected override void PostInit(HappinessAsset asset)
+    {
+        base.PostInit(asset);
+        asset.index = cached_library.list.IndexOf(asset);
+    }
+}
