0631aeea5e27c556d951677e10f8f784d1f33d50
inmny <1342885270@qq.com>
2025-11-01 00:19:49 +0800

    bugfix: 修复不兼容设置国家旗帜的bug

--- Source/Content/Banners.cs
+++ Source/Content/Banners.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Cultiway.Abstract;
+using HarmonyLib;
+using NeoModLoader.General.Game.extensions;
 
 namespace Cultiway.Content;
 
@@ -23,9 +26,26 @@ public class Banners : ICanInit
     }
     public void Init()
     {
-        Load("banners/general_icons", BannerType.Clan, ElementType.Icon);
+        Load("banners/general_icons", BannerType.Kingdom, ElementType.Icon);
+
+        new Harmony("Cultiway.Banners.KingdomPatch").Patch(
+            AccessTools.Method(typeof(KingdomBannerLibrary), nameof(KingdomBannerLibrary.loadNewAssetRuntime)),
+            postfix: new HarmonyMethod(AccessTools.Method(typeof(Banners), nameof(Postfix))));
     }
 
+    private static Dictionary<ElementType, List<string>> appended = new Dictionary<ElementType, List<string>>();
+    private static void Postfix(KingdomBannerLibrary __instance, BannerAsset __result)
+    {
+        if (appended.TryGetValue(ElementType.Icon, out var icons))
+        {
+            __result.icons?.AddRange(icons);
+        }
+
+        if (appended.TryGetValue(ElementType.Background, out var backgrounds))
+        {
+            __result.backgrounds?.AddRange(backgrounds);
+        }
+    }
     private void Load(string folder, BannerType type, ElementType element)
     {
         var sprites = SpriteTextureLoader.getSpriteList(folder);
@@ -55,21 +75,52 @@ public class Banners : ICanInit
                 break;
         }
 
+        if (type == BannerType.Kingdom)
+        {
+            switch (element)
+            {
+                case ElementType.Icon:
+                    if (!appended.TryGetValue(ElementType.Icon, out var icons))
+                    {
+                        icons = new List<string>();
+                        appended.Add(ElementType.Icon, icons);
+                    }
+                    icons.AddRange(paths);
+                    break;
+                case ElementType.Background:
+                    if (!appended.TryGetValue(ElementType.Background, out var backgrounds))
+                    {
+                        backgrounds = new List<string>();
+                        appended.Add(ElementType.Background, backgrounds);
+                    }
+                    backgrounds.AddRange(paths);
+                    break;
+                case ElementType.Frame:
+                    if (!appended.TryGetValue(ElementType.Frame, out var frames))
+                    {
+                        frames = new List<string>();
+                        appended.Add(ElementType.Frame, frames);
+                    }
+                    frames.AddRange(paths);
+                    break;
+            }
+            return;
+        }
+
         if (asset == null)
         {
             return;
         }
-
         switch (element)
         {
             case ElementType.Icon:
-                asset.icons.AddRange(paths);
+                asset.icons?.AddRange(paths);
                 break;
             case ElementType.Background:
-                asset.backgrounds.AddRange(paths);
+                asset.backgrounds?.AddRange(paths);
                 break;
             case ElementType.Frame:
-                asset.frames.AddRange(paths);
+                asset.frames?.AddRange(paths);
                 break;
         }
     }
