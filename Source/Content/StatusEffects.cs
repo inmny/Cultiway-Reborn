361c10a6abff144cab7710395e06ebe59407a8ea
inmny <1342885270@qq.com>
2025-11-23 14:25:39 +0800

    feat: 添加状态粒子系统及相关效果支持

--- Source/Content/StatusEffects.cs
+++ Source/Content/StatusEffects.cs
@@ -1,6 +1,7 @@
 using Cultiway.Abstract;
 using Cultiway.Core.Libraries;
 using strings;
+using UnityEngine;
 
 namespace Cultiway.Content;
 
@@ -10,13 +11,17 @@ public class StatusEffects : ExtendLibrary<StatusEffectAsset, StatusEffects>
     public static StatusEffectAsset Slow { get; private set; }
     protected override void OnInit()
     {
-        Enlighten = StatusEffectAsset.StartBuild(nameof(Enlighten)).SetDuration(60).Build();
+        Enlighten = StatusEffectAsset.StartBuild(nameof(Enlighten))
+            .SetDuration(60)
+            .EnableParticle(new Color(1f, 0.85f, 0.35f), 2, 0.4f)
+            .Build();
         Slow = StatusEffectAsset.StartBuild(nameof(Slow))
             .SetDuration(3f)
             .SetStats(new BaseStats
             {
                 [S.multiplier_speed] = -1f
             })
+            .EnableParticle(new Color(0.4f, 0.6f, 1f), 1, 0.6f)
             .Build();
     }
 }
