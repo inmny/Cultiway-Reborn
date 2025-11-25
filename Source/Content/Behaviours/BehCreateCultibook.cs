849c8f6ae132ac4d3b0a31b110ac78a9bb1d9c4d
inmny <1342885270@qq.com>
2025-11-26 00:20:54 +0800

    fix: 修复创建功法时不会自动设为主修功法

--- Source/Content/Behaviours/BehCreateCultibook.cs
+++ Source/Content/Behaviours/BehCreateCultibook.cs
@@ -11,13 +11,14 @@ public class BehCreateCultibook : BehCityActor
 {
     public override BehResult execute(Actor pObject)
     {
-        var ae = pObject.GetExtend();
         var raw_cultibook = World.world.books.CreateNewCultibook(pObject);
         if (raw_cultibook == null)
         {
             return BehResult.Stop;
         }
         pObject.timer_action = Randy.randomFloat(TimeScales.SecPerYear, TimeScales.SecPerYear * 3);
+        var ae = pObject.GetExtend();
+        ae.SetMainCultibook(raw_cultibook.GetExtend().GetComponent<Cultibook>().Asset);
         return BehResult.Continue;
     }
 }
