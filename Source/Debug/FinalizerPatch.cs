ec61258849c507c0d10e251229e2409afe54247e
inmny <1342885270@qq.com>
2026-02-11 15:29:54 +0800

    bugfix: 修复WindowMetaElement泛型类的Harmony Finalizer补丁
    使用TargetMethods动态查找所有继承自WindowMetaElement<TMetaObject,TData>的具体类并分别对其OnEnable方法应用Finalizer，避免直接修补开放泛型类导致的补丁失败。

--- Source/Debug/FinalizerPatch.cs
+++ Source/Debug/FinalizerPatch.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 using HarmonyLib;
 using NeoModLoader.api.attributes;
 using UnityEngine;
@@ -57,5 +59,51 @@ internal static class FinalizerPatch
             ModClass.LogInfo($"TabHistoryData_getNanoObject: {__instance.id}, {__instance.meta_type}");
         }
         return __exception;
+    }
+    [HarmonyFinalizer, HarmonyPatch(typeof(MultiBannerPool), nameof(MultiBannerPool.getNext))]
+    private static Exception MultiBannerPool_getNext(Exception __exception, MultiBannerPool __instance, NanoObject pObject)
+    {
+        if (__exception != null)
+        {
+            var meta_asset = AssetManager.meta_customization_library.get(pObject.getType());
+            ModClass.LogInfo($"MultiBannerPool_getNext: {__instance._pool_banners == null}, {meta_asset==null}, {meta_asset?.get_banner==null}");
+        }
+        return __exception;
+    }
+    
+    [HarmonyPatch]
+    public static class WindowMetaElementGenericPatch
+    {
+        public static IEnumerable<System.Reflection.MethodBase> TargetMethods()
+        {
+            var type_base = typeof(WindowMetaElement<,>);
+            var assembly = type_base.Assembly;
+            foreach (var type in assembly.GetTypes())
+            {
+                if (type.IsAbstract) continue;
+                var base_type = type.BaseType;
+                while (base_type != null)
+                {
+                    if (base_type.IsGenericType && base_type.GetGenericTypeDefinition() == type_base)
+                    {
+                        var method = AccessTools.Method(type, "OnEnable");
+                        if (method == null) method = AccessTools.Method(base_type, "OnEnable");
+                        if (method != null) yield return method;
+                        break;
+                    }
+                    base_type = base_type.BaseType;
+                }
+            }
+        }
+
+        [HarmonyFinalizer]
+        public static Exception Finalizer(Exception __exception, WindowMetaElementBase __instance)
+        {
+            if (__exception != null)
+            {
+                ModClass.LogInfo($"WindowMetaElement_OnEnable: {__instance?.name ?? "null"}");
+            }
+            return null;
+        }
     }
 }
