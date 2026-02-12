fcacead058d8a2f157456539856f9aacd1344810
inmny <1342885270@qq.com>
2026-02-12 23:07:33 +0800

    feat: 添加 Tile 获取 GeoRegion 的方法

--- Source/Core/TileExtend.cs
+++ Source/Core/TileExtend.cs
@@ -1,5 +1,6 @@
 using Cultiway.Abstract;
 using Cultiway.Core.Components;
+using Cultiway.Core.GeoLib.Components;
 using Friflo.Engine.ECS;
 
 namespace Cultiway.Core;
@@ -14,4 +15,22 @@ public class TileExtend : ExtendComponent<WorldTile>
     }
     public override Entity E => e;
     public override WorldTile Base => e.GetComponent<TileBinder>().Tile;
+
+    public GeoRegion GetGeoRegion()
+    {
+        var rels = e.GetRelations<BelongToRelation>();
+        foreach (var rel in rels)
+        {
+            if (rel.entity.HasComponent<GeoRegionBinder>())
+            {
+                return rel.entity.GetComponent<GeoRegionBinder>().GeoRegion;
+            }
+        }
+        return null;
+    }
+
+    public bool HasGeoRegion()
+    {
+        return GetGeoRegion() != null;
+    }
 }
