d8ec7845875b17745dccce6588d804a9529d1953
inmny <1342885270@qq.com>
2026-02-12 23:08:10 +0800

    feat: 添加 GeoRegion 列表组件

---
+++ Source/UI/Components/GeoRegionListComponent.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cultiway.Core;
+
+namespace Cultiway.UI.Components
+{
+    public class GeoRegionListComponent : ComponentListBase<GeoRegionListElement, GeoRegion, GeoRegionData, GeoRegionListComponent>
+    {
+        
+    }
+}
