691e1b395d374fe64e03010954144810f178e24e
inmny <1342885270@qq.com>
2026-02-12 13:15:28 +0800

    feat: 添加删除 Tab 的扩展方法

--- Source/Utils/Extension/MetaWindowTools.cs
+++ Source/Utils/Extension/MetaWindowTools.cs
@@ -1,4 +1,6 @@
 
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,5 +21,36 @@ public static class MetaWindowTools
         tab_title_obj.Find("icon_right").GetComponent<Image>().sprite = SpriteTextureLoader.getSprite(right_icon_path);
 
         return tab_title_obj.gameObject;
+    }
+    public static void DeleteTab(this TabbedWindow window, string tab_name)
+    {
+        var all_tabs = window.GetComponentsInChildren<WindowMetaTab>(true);
+        var tab = all_tabs.FirstOrDefault(t => t.name == tab_name);
+        if (tab == null) return;
+
+        var elements_to_delete = new List<GameObject>();
+        foreach (var element in tab.tab_elements)
+        {
+            var used = false;
+            foreach (var another_tab in all_tabs)
+            {
+                if (another_tab == tab) continue;
+
+                if (another_tab.tab_elements.Contains(element))
+                {
+                    used = true;
+                    break;
+                }
+            }
+            if (used) continue;
+            
+            elements_to_delete.Add(element.gameObject);
+        }
+        tab.tab_elements.Clear();
+        Object.DestroyImmediate(tab.gameObject);
+        foreach (var element in elements_to_delete)
+        {
+            Object.DestroyImmediate(element);
+        }
     }
 }
