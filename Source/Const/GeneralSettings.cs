bc74352b07e475560b0585458873966e4dd582db
inmny <1342885270@qq.com>
2026-03-02 01:40:26 +0800

    feat: 添加各系统的独立开关配置
    - GeneralSettings 新增功法、符箓、丹药、师徒、技能系统的开关属性
    - default_config.json 添加对应的配置项
    - Locales/config.csv 添加本地化描述

--- Source/Const/GeneralSettings.cs
+++ Source/Const/GeneralSettings.cs
@@ -5,7 +5,31 @@ namespace Cultiway.Const;
 public static class GeneralSettings
 {
     public static float SpawnNaturally { get; private set; } = 0.1f;
+    /// <summary>
+    /// 地理演化
+    /// </summary>
     public static bool EnableGeoSystems { get; private set; } = true;
+    /// <summary>
+    /// 功法
+    /// </summary>
+    public static bool EnableCultibookSystems { get; private set; } = true;
+    /// <summary>
+    /// 符箓
+    /// </summary>
+    public static bool EnableTalismanSystems { get; private set; } = true;
+    
+    /// <summary>
+    /// 丹药
+    /// </summary>
+    public static bool EnableElixirSystems { get; private set; } = true;
+    /// <summary>
+    /// 师徒
+    /// </summary>
+    public static bool EnableAMSystems { get; private set; } = true;
+    /// <summary>
+    /// 技能
+    /// </summary>
+    public static bool EnableSkillSystems { get; private set; } = true;
     public static void SetElementRootSpawnNaturally(float value)
     {
         SpawnNaturally = value;
@@ -14,6 +38,29 @@ public static class GeneralSettings
     public static void SwitchGeoSystems(bool value)
     {
         EnableGeoSystems = value;
+    }
+
+    public static void SwitchCultibookSystems(bool value)
+    {
+        EnableCultibookSystems = value;
+    }
+    public static void SwitchTalismanSystems(bool value)
+    {
+        EnableTalismanSystems = value;
+    }
+
+    public static void SwitchElixirSystems(bool value)
+    {
+        EnableElixirSystems = value;
+    }
+
+    public static void SwitchAMSystems(bool value)
+    {
+        EnableAMSystems = value;
+    }
+    public static void SwitchSkillSystems(bool value)
+    {
+        EnableSkillSystems = value;
     }
 
     public static void SwitchTrainExperimentalTimedDispatch(bool value)
