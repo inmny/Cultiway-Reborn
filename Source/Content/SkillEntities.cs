a50716a68b998b58b190c86c4ed5f1192ffb69ad
inmny <1342885270@qq.com>
2026-07-02 13:56:47 +0800

    fix: 修复木刺术没有默认轨迹的bug

--- Source/Content/SkillEntities.cs
+++ Source/Content/SkillEntities.cs
@@ -145,6 +145,7 @@ public class SkillEntities : ExtendLibrary<SkillEntityAsset, SkillEntities>
                 Enemy = true,
                 Actor = true
             })
+            .SetupDefaultTraj(SkillTrajectories.TowardsDirection)
             .OnObjCollision = (ref SkillContext context, Entity skill_container, Entity entity,
             BaseSimObject target) =>
         {
