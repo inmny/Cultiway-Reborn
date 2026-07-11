b96051456781095b4256ce43fe9a6f332e1c91cd
inmny <1342885270@qq.com>
2026-07-11 20:06:24 +0800

    feat: 在收卸法器的时候自动更新状态和装配关系

---
+++ Source/Content/ArtifactEquipmentManager.cs
@@ -0,0 +1,52 @@
+using Cultiway.Abstract;
+using Cultiway.Content.Components;
+using Cultiway.Core;
+using Friflo.Engine.ECS;
+
+namespace Cultiway.Content;
+
+public class ArtifactEquipmentManager : ICanInit
+{
+    public void Init()
+    {
+        InventoryLifecycle.RegisterBeforeItemAdded(RemoveEquipmentBeforeTransfer);
+        InventoryLifecycle.RegisterAfterItemAdded(MarkArtifactCarrier);
+        InventoryLifecycle.RegisterBeforeItemExtracted(RemoveEquipmentBeforeExtraction);
+        InventoryLifecycle.RegisterAfterItemExtracted(ClearArtifactCarrier);
+    }
+
+    private static void RemoveEquipmentBeforeTransfer(IHasInventory _, Entity item)
+    {
+        using ListPool<Entity> owners = new(item.GetIncomingLinks<EquippedArtifactRelation>().Entities);
+        foreach (Entity owner in owners)
+        {
+            owner.RemoveRelation<EquippedArtifactRelation>(item);
+        }
+    }
+
+    private static void MarkArtifactCarrier(IHasInventory inventory, Entity item)
+    {
+        if (inventory is not ActorExtend actor || !item.HasComponent<Artifact>()) return;
+        if (!actor.E.HasComponent<ArtifactLoadoutState>())
+        {
+            actor.E.AddComponent(new ArtifactLoadoutState());
+        }
+    }
+
+    private static void RemoveEquipmentBeforeExtraction(IHasInventory inventory, Entity item)
+    {
+        if (inventory is ActorExtend actor)
+        {
+            actor.E.RemoveRelation<EquippedArtifactRelation>(item);
+        }
+    }
+
+    private static void ClearArtifactCarrier(IHasInventory inventory, Entity _)
+    {
+        if (inventory is not ActorExtend actor || actor.HasItem<Artifact>()) return;
+        if (actor.E.HasComponent<ArtifactLoadoutState>())
+        {
+            actor.E.RemoveComponent<ArtifactLoadoutState>();
+        }
+    }
+}
