ef5e517e6bcefcad8af127163a781d4a3680d6f3
inmny <1342885270@qq.com>
2026-07-11 20:26:47 +0800

    feat: 添加法器自动调度系统

--- Source/Content/Manager.cs
+++ Source/Content/Manager.cs
@@ -58,6 +58,7 @@ internal class Manager
         ModClass.I.GeneralLogicSystems.Add(new TrainTransportSystem());
         ModClass.I.GeneralLogicSystems.Add(new TeleportArraySystem());
         ModClass.I.GeneralLogicSystems.Add(new CityDistributeItemsSystem());
+        ModClass.I.GeneralLogicSystems.Add(new ArtifactEquipmentSystem());
         ModClass.I.GeneralLogicSystems.Add(new ContinuousCultivateSystem());
         ModClass.I.GeneralLogicSystems.Add(new SectConstructionSystem());
         ActorExtend.RegisterActionOnDeath(SectTreasureRules.ReturnBorrowedTreasuresOnDeath);
---
+++ Source/Content/Systems/Logic/ArtifactEquipmentSystem.cs
@@ -0,0 +1,47 @@
+using Cultiway.Const;
+using Cultiway.Content.Artifacts;
+using Cultiway.Content.Components;
+using Cultiway.Content.Extensions;
+using Cultiway.Content.Libraries;
+using Cultiway.Core;
+using Cultiway.Core.Components;
+using Friflo.Engine.ECS;
+using Friflo.Engine.ECS.Systems;
+using UnityEngine;
+
+namespace Cultiway.Content.Systems.Logic;
+
+public class ArtifactEquipmentSystem : QuerySystem<ActorBinder, ArtifactLoadoutState>
+{
+    private const float EvaluationInterval = 2f;
+    private double _lastEvaluationTime = double.MinValue;
+
+    protected override void OnUpdate()
+    {
+        if (!GeneralSettings.EnableArtifactSystems) return;
+
+        double now = World.world.getCurSessionTime();
+        if (_lastEvaluationTime != double.MinValue && now - _lastEvaluationTime < EvaluationInterval) return;
+        
+        float elapsed = _lastEvaluationTime == double.MinValue
+            ? 0f
+            : Mathf.Clamp((float)(now - _lastEvaluationTime), 0f, 10f);
+        _lastEvaluationTime = now;
+
+        using ListPool<ActorExtend> actors = new();
+        Query.ForEachEntity((ref ActorBinder binder, ref ArtifactLoadoutState _, Entity _) =>
+        {
+            Actor actor = binder.Actor;
+            if (actor != null && actor.isAlive()) actors.Add(binder.AE);
+        });
+
+        foreach (ActorExtend actor in actors)
+        {
+            ArtifactLoadoutPlanner.Refresh(actor, true, elapsed);
+            if (!actor.HasItem<Artifact>())
+            {
+                actor.E.RemoveComponent<ArtifactLoadoutState>();
+            }
+        }
+    }
+}
