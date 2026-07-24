20801c93e88065f733f000827212910be93d0b58
inmny <1342885270@qq.com>
2026-07-24 21:50:19 +0800

    bugfix: 避免加载期创建零尺寸渲染纹理

--- Source/Patch/PatchFixVanillaBugs.cs
+++ Source/Patch/PatchFixVanillaBugs.cs
@@ -2,11 +2,20 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
+using UnityEngine;
 
 namespace Cultiway.Patch;
 
 internal static class PatchFixVanillaBugs
 {
+    [HarmonyPrefix, HarmonyPatch(typeof(EffectsCamera), "LateUpdate")]
+    private static bool EffectsCamera_LateUpdate()
+    {
+        // 原版会把屏幕尺寸除以 3 后直接创建 RenderTexture；
+        // 加载早期尺寸可能暂时为 0，此时等待下一帧尺寸就绪即可。
+        return Screen.width >= 3 && Screen.height >= 3;
+    }
+
     [HarmonyTranspiler, HarmonyPatch(typeof(Actor), nameof(Actor.updateRotations))]
     private static IEnumerable<CodeInstruction> Actor_updateRotations(IEnumerable<CodeInstruction> codes)
     {
